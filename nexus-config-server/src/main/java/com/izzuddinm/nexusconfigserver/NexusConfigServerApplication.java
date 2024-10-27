ac1e243b4c84603e69c27cd22fcc0f859960a84a
Muhammad Ayom Izzudin <ayom.izzudin@gtech.digital>
2024-10-27 14:23:45 +0700

    [SPRING-CLOUD] Initial commit - learn spring cloud

---
+++ nexus-config-server/src/main/java/com/izzuddinm/nexusconfigserver/NexusConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.izzuddinm.nexusconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusConfigServerApplication.class, args);
+	}
+
+}
---
+++ nexus-config-server/src/test/java/com/izzuddinm/nexusconfigserver/NexusConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.izzuddinm.nexusconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
