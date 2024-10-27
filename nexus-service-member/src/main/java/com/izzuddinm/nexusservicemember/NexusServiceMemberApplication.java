2d5cf462a51bcbf7b3e95566b4e7e3a861f838b3
Muhammad Ayom Izzudin <ayom.izzudin@gtech.digital>
2024-10-27 19:50:52 +0700

    [SPRING-CLOUD] init service member

---
+++ nexus-service-member/src/main/java/com/izzuddinm/nexusservicemember/NexusServiceMemberApplication.java
@@ -0,0 +1,13 @@
+package com.izzuddinm.nexusservicemember;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusServiceMemberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusServiceMemberApplication.class, args);
+	}
+
+}
