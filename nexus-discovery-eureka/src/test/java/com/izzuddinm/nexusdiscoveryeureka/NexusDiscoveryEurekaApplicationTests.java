8ee8670bd4cc07d7941758d2f325799e246df1f5
Muhammad Ayom Izzudin <ayom.izzudin@gtech.digital>
2024-10-27 15:55:01 +0700

    [SPRING-CLOUD] initial commit eureka server

---
+++ nexus-discovery-eureka/src/test/java/com/izzuddinm/nexusdiscoveryeureka/NexusDiscoveryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.izzuddinm.nexusdiscoveryeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusDiscoveryEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
